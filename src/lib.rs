f4e0a3b6937aa737b0aea270b9da1934b2ea3641
Akbar Fadiansyah <akbarfadi01@gmail.com>
2024-01-25 21:23:53 +1100

    More docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,41 @@
 //! A Rust wrapper for [isEven API](https://isevenapi.xyz/).
 //!
 //! Includes the library as well as a simple command line app.
+//!
+//! # Examples
+//! A simple commandline app:
+//! ```
+//! use std::process::exit;
+//! use iseven_api::IsEvenApiBlockingClient;
+//!
+//! const USAGE_MSG: &str = "Usage: iseven_api [integer]";
+//!
+//! fn main() {
+//!     let argv = std::env::args().collect::<Vec<_>>();
+//!     let app_name = &argv[0];
+//!     if argv.len() != 2 {
+//!         eprintln!("error: {}: {}", app_name, USAGE_MSG);
+//!         exit(1);
+//!     } else {
+//!         let num = &argv[1];
+//!         let client = IsEvenApiBlockingClient::new();
+//!         match client.get(num) {
+//!             Ok(response) => {
+//!                 println!("Advertisement: {}", response.ad());
+//!                 println!(
+//!                     "{} is an {} number",
+//!                     num,
+//!                     if response.iseven() { "even" } else { "odd" }
+//!                 )
+//!             }
+//!             Err(e) => {
+//!                 eprintln!("error: {}: {}", app_name, e);
+//!                 exit(1);
+//!             }
+//!         }
+//!     }
+//! }
+//! ```
 
 use std::fmt::{Display, Formatter};
 
@@ -11,14 +46,33 @@ const API_URL: &str = "https://api.isevenapi.xyz/api/iseven/";
 
 /// Checks if a number is even.
 ///
-/// **Note:** this method will panic if it encounters an error.
+/// **Note:** this method will panic if it encounters an error. Use [`IsEvenApiClient`] or [`IsEvenApiBlockingClient`]
+/// if you want to handle failures more gracefully.
+///
+/// # Examples
+/// ```
+/// use iseven_api::is_even;
+///
+/// # fn main() {
+/// assert!(is_even(42));
+/// # }
 pub fn is_even<T: Display>(number: T) -> bool {
     IsEvenApiBlockingClient::new().get(number).unwrap().iseven()
 }
 
 /// Checks if a number is odd.
 ///
-/// **Note:** this method will panic if it encounters an error.
+///
+/// **Note:** this method will panic if it encounters an error. Use [`IsEvenApiClient`] or [`IsEvenApiBlockingClient`]
+/// if you want to handle failures more gracefully.
+///
+/// # Examples
+/// ```
+/// use iseven_api::is_odd;
+///
+/// # fn main() {
+/// assert!(is_odd(333));
+/// # }
 pub fn is_odd<T: Display>(number: T) -> bool {
     !is_even(number)
 }
