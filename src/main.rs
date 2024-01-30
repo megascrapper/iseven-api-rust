4890bbdf08af6187e91700b3b6a374b0b03de46e
Akbar Fadiansyah <akbarfadi01@gmail.com>
2024-01-31 10:20:47 +1100

    Accept multiple inputs on CLI

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,17 @@ use iseven_api::IsEvenApiBlockingClient;
 #[derive(Parser)]
 struct Cli {
     /// Number to check
-    number: String,
+    numbers: Vec<String>,
 
     /// Print JSON response
     #[arg(long)]
-    json: bool
+    json: bool,
 }
 
-fn print_error<T: Display>(msg: T) -> ! {
+fn print_error<M: Display, V: Display>(msg: M, input_value: V) -> ! {
     let argv = std::env::args().collect::<Vec<_>>();
     let app_name = &argv[0];
-    eprintln!("error: {}: {}", app_name, msg);
+    eprintln!("error: {}: {}: {}", app_name, input_value, msg);
     exit(1)
 }
 
@@ -26,30 +26,26 @@ fn main() {
     env_logger::init();
     setup_panic!();
     let cli = Cli::parse();
-    let num = cli.number;
-
     let client = IsEvenApiBlockingClient::new();
-    if cli.json {
-        match client.get_json(&num) {
-            Ok(response) => {
-                println!("{}", response)
+    for num in cli.numbers {
+        if cli.json {
+            match client.get_json(&num) {
+                Ok(response) => {
+                    println!("{}", response)
+                }
+                Err(e) => print_error(e, num),
             }
-            Err(e) => {
-                print_error(e)
-            }
-        }
-    } else {
-        match client.get(&num) {
-            Ok(response) => {
-                println!("Advertisement: {}", response.ad());
-                println!(
-                    "{} is an {} number",
-                    &num,
-                    if response.iseven() { "even" } else { "odd" }
-                )
-            }
-            Err(e) => {
-                print_error(e)
+        } else {
+            match client.get(&num) {
+                Ok(response) => {
+                    println!("Advertisement: {}", response.ad());
+                    println!(
+                        "{} is an {} number",
+                        &num,
+                        if response.iseven() { "even" } else { "odd" }
+                    )
+                }
+                Err(e) => print_error(e, num),
             }
         }
     }
